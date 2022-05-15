dc13d0e03cff746e5a95c0f15432b229766b9cca
BaxterNickolai <a1827764@student.adelaide.edu.au>
2022-05-15 03:59:56 +0000

    comments

--- Player.h
+++ Player.h
@@ -4,17 +4,17 @@
 #include <string>
 class Player{
     public:
-    player();
-    void setName(std::string playerName);
-    std::string getName();
-    void setStats();
+    player(); //initalised attributes (ints and string)
+    void setName(std::string playerName); //sets the name of the player
+    std::string getName(); //returns the name of the player
+    void setStats(); //sets the stats of the player based on what the user wants to input into each stat
     
-    int damage;
-    int maxHealth;
-    int critChance;
-    int dodge;
-    int points;
-    int xp;
-    int level;
-    std::string name;
+    int damage; //damage level of player
+    int maxHealth; //maximum health level of player
+    int critChance; //critical chance level of player
+    int dodge; //dodge level of player
+    int points; //how many points are available for the player to increase their stats level (damage, maxHealth, critCahnce and dodge)
+    int xp; //the xp of the character (contributes to the level)
+    int level; //the level the character is on, which increases based on xp, affected by combat and affects stats level
+    std::string name; //the name of player
 }
