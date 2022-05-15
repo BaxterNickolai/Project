b6ef6902790e35fb7f42e9c08111e4309a18656b
Denis Vasilyev <a1794650@student.adelaide.edu.au>
2022-05-15 13:22:54 +0000

    Fixed Monster, it now compiles

--- Monster.cpp
+++ Monster.cpp
@@ -1,120 +1,122 @@
 #include <iostream>
+#include <math.h>
 #include "Monster.h"
 #include "Player.h"
 using namespace std;
 
-Monster::monster() {
+Monster::Monster() {
     //initalising the starting values for the attributes contained in Monster.h
     mdamage = 0;
-    mmaxHealth = 0;
+    maxHealth = 0;
     mcritChance = 0;
     mdodge = 0;
     mname = "?";
+    level = 1;
 }
 
 void Monster::setMonster() {
-    int a = rand(1,20);
+    int a = 1+( rand() % 20 );
     switch (a) {
-        case 1: mname = "Goblin"
-            mdamage = level^1.25 + 1;
-            mmaxHealth = level^1.25 + 1;
-            mcritCahnce = level^1.25 + 3;
-            mdodge = level^1.25 + 5;
-        case 2: mname = "Golem"
-            mdamage = level^1.25 + 3;
-            mmaxHealth = level^1.25 + 5;
-            mcritCahnce = level^1.25 + 3;
-            mdodge = level^1.25 + 1;
-        case 3: mname = "Assassin"
-            mdamage = level^1.25 + 1;
-            mmaxHealth = level^1.25 + 1;
-            mcritCahnce = level^1.25 + 3;
-            mdodge = level^1.25 + 5;
-        case 4: mname = "Baron"
-            mdamage = level^1.25 + 3;
-            mmaxHealth = level^1.25 + 3;
-            mcritCahnce = level^1.25  1;
-            mdodge = level^1.25 + 3;
-        case 5: mname = "Knight"
-            mdamage = level^1.25 + 4;
-            mmaxHealth = level^1.25 + 3;
-            mcritCahnce = level^1.25 + 3;
-            mdodge = level^1.25 + 1;
-        case 6: mname = "Orc"
-            mdamage = level^1.25 + 2;
-            mmaxHealth = level^1.25 + 2;
-            mcritCahnce = level^1.25 + 5;
-            mdodge = level^1.25 + 1;
-        case 7: mname = "Ogre"
-            mdamage = level^1.25 + 2;
-            mmaxHealth = level^1.25 + 5;
-            mcritCahnce = level^1.25 + 2;
-            mdodge = level^1.25 + 1;
-        case 8: mname = "Paladin"
-            mdamage = level^1.25 + 4;
-            mmaxHealth = level^1.25 + 2;
-            mcritCahnce = level^1.25 + 4;
-            mdodge = level^1.25 + 2;
-        case 9: mname = "Wisp"
-            mdamage = level^1.25 + 1;
-            mmaxHealth = level^1.25 + 1;
-            mcritCahnce = level^1.25 + 1;
-            mdodge = level^1.25 + 1;
-        case 10: mname = "Mummy"
-            mdamage = level^1.25 + 1;
-            mmaxHealth = level^1.25 + 1;
-            mcritCahnce = level^1.25 + 5;
-            mdodge = level^1.25 + 3;
-        case 11: mname = "Wyvern"
-            mdamage = level^1.25 + 2;
-            mmaxHealth = level^1.25 + 4;
-            mcritCahnce = level^1.25 + 1;
-            mdodge = level^1.25 + 2;
-        case 12: mname = "Wizard"
-            mdamage = level^1.25 + 3;
-            mmaxHealth = level^1.25 + 1;
-            mcritCahnce = level^1.25 + 5;
-            mdodge = level^1.25 + 2;
-        case 13: mname = "Dragon"
-            mdamage = level^1.25 + 5;
-            mmaxHealth = level^1.25 + 5;
-            mcritCahnce = level^1.25 + 1;
-            mdodge = level^1.25 + 1;
-        case 14: mname = "Griffen"
-            mdamage = level^1.25 + 1;
-            mmaxHealth = level^1.25 + 1;
-            mcritCahnce = level^1.25 + 1;
-            mdodge = level^1.25 + 5;
-        case 15: mname = "Angel"
-            mdamage = level^1.25 + 1;
-            mmaxHealth = level^1.25 + 5;
-            mcritCahnce = level^1.25 + 1;
-            mdodge = level^1.25 + 5;
-        case 16: mname = "Demon"
-            mdamage = level^1.25 + 4;
-            mmaxHealth = level^1.25 + 1;
-            mcritCahnce = level^1.25 + 4;
-            mdodge = level^1.25 + 1;
-        case 17: mname = "Rock Elemental"
-            mdamage = level^1.25 + 3;
-            mmaxHealth = level^1.25 + 3;
-            mcritCahnce = level^1.25 + 3;
-            mdodge = level^1.25 + 1;
-        case 18: mname = "Leprechaun"
-            mdamage = level^1.25 + 1;
-            mmaxHealth = level^1.25 + 3;
-            mcritCahnce = level^1.25 + 1;
-            mdodge = level^1.25 + 5;
-        case 19: mname = "Giant"
-            mdamage = level^1.25 + 4;
-            mmaxHealth = level^1.25 + 5;
-            mcritCahnce = level^1.25 + 3;
-            mdodge = level^1.25 + 1;
-        case 20: mname = "Jeff's Mum"
-            mdamage = level^1.25;
-            mmaxHealth = level^1.25 + 5;
-            mcritCahnce = level^1.25;
-            mdodge = level^1.25 + 1;
+        case 1: mname = "Goblin";
+            mdamage = pow(level, 1.25) + 1;
+            maxHealth = pow(level, 1.25) + 1; 
+            mcritChance = pow(level, 1.25) + 3;
+            mdodge = pow(level, 1.25) + 5;
+        case 2: mname = "Golem";
+            mdamage = pow(level, 1.25) + 3;
+            maxHealth = pow(level, 1.25) + 5;
+            mcritChance = pow(level, 1.25) + 3;
+            mdodge = pow(level, 1.25) + 1;
+        case 3: mname = "Assassin";
+            mdamage = pow(level, 1.25) + 1;
+            maxHealth = pow(level, 1.25) + 1;
+            mcritChance = pow(level, 1.25) + 3;
+            mdodge = pow(level, 1.25) + 5;
+        case 4: mname = "Baron";
+            mdamage = pow(level, 1.25) + 3;
+            maxHealth = pow(level, 1.25) + 3;
+            mcritChance = pow(level, 1.25) + 1;
+            mdodge = pow(level, 1.25) + 3;
+        case 5: mname = "Knight";
+            mdamage = pow(level, 1.25) + 4;
+            maxHealth = pow(level, 1.25) + 3;
+            mcritChance = pow(level, 1.25) + 3;
+            mdodge = pow(level, 1.25) + 1;
+        case 6: mname = "Orc";
+            mdamage = pow(level, 1.25) + 2;
+            maxHealth = pow(level, 1.25) + 2;
+            mcritChance = pow(level, 1.25) + 5;
+            mdodge = pow(level, 1.25) + 1;
+        case 7: mname = "Ogre";
+            mdamage = pow(level, 1.25) + 2;
+            maxHealth = pow(level, 1.25) + 5;
+            mcritChance = pow(level, 1.25) + 2;
+            mdodge = pow(level, 1.25) + 1;
+        case 8: mname = "Paladin";
+            mdamage = pow(level, 1.25) + 4;
+            maxHealth = pow(level, 1.25) + 2;
+            mcritChance = pow(level, 1.25) + 4;
+            mdodge = pow(level, 1.25) + 2;
+        case 9: mname = "Wisp";
+            mdamage = pow(level, 1.25) + 1;
+            maxHealth = pow(level, 1.25) + 1;
+            mcritChance = pow(level, 1.25) + 1;
+            mdodge = pow(level, 1.25) + 1;
+        case 10: mname = "Mummy";
+            mdamage = pow(level, 1.25) + 1;
+            maxHealth = pow(level, 1.25) + 1;
+            mcritChance = pow(level, 1.25) + 5;
+            mdodge = pow(level, 1.25) + 3;
+        case 11: mname = "Wyvern";
+            mdamage = pow(level, 1.25) + 2;
+            maxHealth = pow(level, 1.25) + 4;
+            mcritChance = pow(level, 1.25) + 1;
+            mdodge = pow(level, 1.25) + 2;
+        case 12: mname = "Wizard";
+            mdamage = pow(level, 1.25) + 3;
+            maxHealth = pow(level, 1.25) + 1;
+            mcritChance = pow(level, 1.25) + 5;
+            mdodge = pow(level, 1.25) + 2;
+        case 13: mname = "Dragon";
+            mdamage = pow(level, 1.25) + 5;
+            maxHealth = pow(level, 1.25) + 5;
+            mcritChance = pow(level, 1.25) + 1;
+            mdodge = pow(level, 1.25) + 1;
+        case 14: mname = "Griffen";
+            mdamage = pow(level, 1.25) + 1;
+            maxHealth = pow(level, 1.25) + 1;
+            mcritChance = pow(level, 1.25) + 1;
+            mdodge = pow(level, 1.25) + 5;
+        case 15: mname = "Angel";
+            mdamage = pow(level, 1.25) + 1;
+            maxHealth = pow(level, 1.25) + 5;
+            mcritChance = pow(level, 1.25) + 1;
+            mdodge = pow(level, 1.25) + 5;
+        case 16: mname = "Demon";
+            mdamage = pow(level, 1.25) + 4;
+            maxHealth = pow(level, 1.25) + 1;
+            mcritChance = pow(level, 1.25) + 4;
+            mdodge = pow(level, 1.25) + 1;
+        case 17: mname = "Rock Elemental";
+            mdamage = pow(level, 1.25) + 3;
+            maxHealth = pow(level, 1.25) + 3;
+            mcritChance = pow(level, 1.25) + 3;
+            mdodge = pow(level, 1.25) + 1;
+        case 18: mname = "Leprechaun";
+            mdamage = pow(level, 1.25) + 1;
+            maxHealth = pow(level, 1.25) + 3;
+            mcritChance = pow(level, 1.25) + 1;
+            mdodge = pow(level, 1.25) + 5;
+        case 19: mname = "Giant";
+            mdamage = pow(level, 1.25) + 4;
+            maxHealth = pow(level, 1.25) + 5;
+            mcritChance = pow(level, 1.25) + 3;
+            mdodge = pow(level, 1.25) + 1;
+        case 20: mname = "Jeff's Mum";
+            mdamage = pow(level, 1.25);
+            maxHealth = pow(level, 1.25) + 5;
+            mcritChance = pow(level, 1.25);
+            mdodge = pow(level, 1.25) + 1;
     }
 }
 
@@ -124,5 +126,5 @@ std::string Monster::getNameMonster() {
 }
 
 int Monster::mDamageCalculator() {
-    
+    return 0;
 }
--- Monster.h
+++ Monster.h
@@ -4,14 +4,16 @@
 #include <string>
 class Monster{
     public:
-    monster(); //initalised attributes (ints and string)
+    Monster(); //initalised attributes (ints and string)
     void setMonster(); //sets monsters stats
     std::string getNameMonster(); //returns name of the monster
     int mDamageCalculator(); //damage calculator for the monster
     
     int mdamage; //damage of the monster
-    int mmaxHealth; //maxmimum health of the monster
+    int maxHealth; //maxmimum health of the monster
     int mcritChance; //critical chance of the monster
     int mdodge; //dodge chance of the monster
+    int level; //level of the monster
     std::string mname; //name of the monster
-}
+};
+#endif //MONSTER_H_
