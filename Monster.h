e87de376d72097dfe182a870336de90c42aaaf1c
BaxterNickolai <a1827764@student.adelaide.edu.au>
2022-05-15 04:04:53 +0000

    Monster.h completed with comments

--- Monster.h
+++ Monster.h
@@ -4,11 +4,14 @@
 #include <string>
 class Monster{
     public:
-    int mDamageCalculator();
+    monster(); //initalised attributes (ints and string)
+    void setNameMonster(std::string monsterName); //sets the name of the player
+    std::string getNameMonster(); //returns name of the monster
+    int mDamageCalculator(); //damage calculator for the monster
     
-    int damage;
-    int maxHealth;
-    int critChance;
-    int dodge;
-    std::string name;
+    int damage; //damage of the monster
+    int maxHealth; //maxmimum health of the monster
+    int critChance; //critical chance of the monster
+    int dodge; //dodge chance of the monster
+    std::string name; //name of the monster
 }
