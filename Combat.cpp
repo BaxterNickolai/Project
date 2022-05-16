eb2638c79ce35ce032a2d45ec3f2bc5fc3ffafa3
BaxterNickolai <a1827764@student.adelaide.edu.au>
2022-05-16 01:34:25 +0000

    combat.cpp expansion 2

--- Combat.cpp
+++ Combat.cpp
@@ -51,36 +51,45 @@ void Combat::fight(){
         turncount++;
         Combat::UserInterface();
         int move = SelectMove();
-        if (monster->dodge>player->dodge) {
-            hpPlayer = hpPlayer-monster->damage;
-            hpMonster = hpMonster-player->damage*move;
-        } else {
-            hpMonster = hpMonster-player->damage*move;
-            int damageDealt = player->damage*move;
-            if(hpMonster>0){
+        if (0<move<4) {
+            //if the player chooses to attack
+            if (monster->dodge>player->dodge) {
+                //if the monster has more dodge stats than the player they attack first
                 hpPlayer = hpPlayer-monster->damage;
-            }
+                hpMonster = hpMonster-player->damage*move;
+            } else {
+                //otherwise the player attacks first
+                hpMonster = hpMonster-player->damage*move;
+                int damageDealt = player->damage*move;
+                if(hpMonster>0){
+                    hpPlayer = hpPlayer-monster->damage;
+                }
+            } 
+        } else {
+            //if the player chooses to dodge
+            
         }
+        if(hpPlayer < 1) {
+            //if player is dead displays game over message, later to be replaced by some sort of end screen with stats
+            std::cout<<"game over";
+            break;
+        } else if (hpMonster < 1) {
+            Combat::PostCombat();
         }
-    }
-    //if player is dead displays game over message, later to be replaced by some sort of end screen with stats
-    if(hpPlayer <1){
-        std::cout<<"game over";
-    }
-    //if player is not dead, post combat activates, allowing player to level up and stuff. Postcombat then initiates precombat again, starting a new fight
-    else{
-        Combat::PostCombat();
     }
 }
 
 void Combat::UserInterface(){
     system("clear");
     std::cout<<"A "<<monster->name<<" has appeared!"<<std::endl;
-    sleep(2);
     if (monster->dodge>player->dodge) {
+        sleep(1);
         std::cout<<"The "<<monster->name<<" caught you off guard, attacking first and dealing "<<monster-damage<<" damage!"<<std::endl<<std::endl;
+        return 1;
     } else {
+        sleep(2);
         std::cout<<"The "<<monster->name<<" is preparing to attack!"<<std::endl<<std::endl;
+        return 2;
     }
 }
 
@@ -108,3 +117,4 @@ int Combat::SelectMove(){
         std::cout<<"Not a vaild input, enter number between 1 and 4."<<std::endl;
         //delay of 3 seconds
         sleep(3);
+    }
